b16df115f50e8d776c0b6ee36651d74928b10bac
Hrishikesh <hrishigaonkar2@gmail.com>
2025-07-23 10:57:32 +0530

    not found page

--- src/pages/WanderLust.js
+++ src/pages/WanderLust.js
@@ -6,7 +6,7 @@ const WanderLust = () => {
       <h1 className='mb-4'>WanderLust</h1>
       <img src='media\wanderlust.png' style={{width:"50rem",borderRadius:"1rem" ,marginTop:"2rem"}}/>
       <p  style={{marginTop:"4rem",fontSize:"1.1rem",lineHeight:"2rem"}}>Wanderlust is a full-stack hotel booking web application inspired by Airbnb. It allows users to browse available <br/> rental properties, make bookings, and list their own properties for rent. The platform includes features like user  <br/>authentication, property listing with images and descriptions, and a booking system. It is built using Node.js, <br/>Express.js, MongoDB, MySQL, and EJS templating, with a frontend styled using HTML, CSS, and Bootstrap.  <br/>The application is deployed on Render, making it accessible online for testing and demonstration.</p>
-      <p style={{fontFamily:"Georgia",fontSize:"1.2rem"}}>Live Link :- </p> <a href=' https://wanderlust-t9iy.onrender.com'> https://wanderlust-t9iy.onrender.com</a>
+      <p style={{fontFamily:"Georgia",fontSize:"1.2rem"}}>Live Link :- </p> <a target="_blank" href=' https://wanderlust-t9iy.onrender.com'> https://wanderlust-t9iy.onrender.com</a>
     </div>
   )
 }
